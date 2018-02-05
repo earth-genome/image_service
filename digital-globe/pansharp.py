028e39f60d37ec71ea5537a94274e166f242a12d
Ed Boyda <ed@space.ninja>
2018-02-05 12:36:36 -0800

    added histogram equalization, cleaned up code

--- digital-globe/pansharp.py
+++ digital-globe/pansharp.py
@@ -3,25 +3,44 @@ and Catalog ID.
 
 Ref: http://gbdxtools.readthedocs.io/en/latest/image_classes.html
 
-Image specs:  Pansharpened, atmosphere corrected, RGB bands
+Image specs (default):  Pansharpened, atmosphere corrected, RGB bands
 
 Inputs: CatalogID, bounding box, flag option to equalize histograms
     Note: flag is set with @ in place of - to allow input of negative numbers
 
-Output: PNG file.
+Output: PNG file(s). (With @e flag, both original and equalized are saved.)
 
 Usage: python pansharp_grab.py CatalogID  bounding box [@e]
 
 Ex: python pansharp_grab.py 1040010034CDD100 151.269378, -33.898346, 151.286458, -33.886092 @e
 """
 
+import numpy as np
 import argparse
+import cv2
+import skimage
 import matplotlib.pyplot as plt
 from gbdxtools import CatalogImage
 
-# TODO, using cv2 tools
-def equilize_histogram(img):
-    return
+def grab_rgb(catalog_id, bbox, pansharpen=True, acomp=True):
+    """Grab an image from GBDX CatalogImage. Return the RGB."""
+    img = CatalogImage(
+        catalog_id, 
+        bbox=bbox, 
+        pansharpen=pansharpen,
+        acomp=acomp
+    )   
+    return img.rgb()
+
+    
+def equalize_histogram(img):
+    """Equalize histogram of input image."""
+    if img.dtype is not np.dtype('uint8'):
+        img = skimage.img_as_ubyte(img)
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    hsv[:,:,2] = cv2.equalizeHist(hsv[:,:,2])
+    equalized = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+    return equalized
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prefix_chars='@',
@@ -38,26 +57,21 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '@e', '@@equalize_histogram',
-        dest='equilize',
+        dest='equalize',
         action='store_true',
         help='Flag (True if set / no value required.)'
     )
     args = parser.parse_args()
     bbox = [float(i.split(',')[0]) for i in args.bounding_box]
     outfile = args.catalog_id + '_LL{:.6f}_{:.6f}'.format(*bbox[:2])
-
-    img = CatalogImage(
+    rgb = grab_rgb(
         args.catalog_id, 
         bbox=bbox, 
         pansharpen=True,
         acomp=True
     )
-    
-    rgb = img.rgb()
-    
-    if args.equilize:
-        rgb = equilize_histogram(rgb)
-        outfile = outfile + '-equi'
-    
     plt.imsave(outfile + '.png', rgb)
+    if args.equalize:
+        rgb = equalize_histogram(rgb)
+        plt.imsave(outfile+'-equi.png', rgb)
    
