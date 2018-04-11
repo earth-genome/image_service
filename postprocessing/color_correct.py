eb1fab9d87fb5373e6377fb757a1c8a26916864b
Ed Boyda <ed@space.ninja>
2018-04-11 15:00:58 -0700

    major rework to take histogram percentiles only as initial reference values, subsequently shifted, to determine black/white points

--- postprocessing/color_correct.py
+++ postprocessing/color_correct.py
@@ -1,13 +1,12 @@
 """Functions for automated basic color correction on an image.
 
-External functions:
-    white_pt_black_pt: Rescale given percentiles of the image histogram
-        to white and black.
-    adjust_contrast: Peform gamma adjustment.
-    color_bright_pts: For each color channel separately, rescale given
-        percentile of histogram to maximum brightness.
+The three basic correction processes are embedded in the internal methods 
+    _expand_histogram: Expand histogram reference values to white and black.
+    _adjust_contrast: Peform gamma adjustment.
+    _balance_color: Shift reference value to max brightness for each color
+        channel.
 
-These functions are packaged into the class:
+These functions are packaged into:
 
 Class ColorCorrect:  Perform basic color correction on an image.
 
@@ -19,17 +18,17 @@ Or if color will be corrected later by hand:
 > cc = ColorCorrect()
 > corrected = cc.brightness_and_contrast(image)
 
+Argument image:  numpy array of type 'uint8' or 'float32', with assumed
+    standard maximum values for images (255 for 'uint8', 1.0 for 'float32')
+
 Notes:
 
-If black_pt_white_pt() is applied with default percentiles, histogram clipping
-is minimal.  That and gamma adjustment are largely reversible.  These two operations are packaged into ColorCorrect.brightness_and_contrast(). On the other
-hand, color_bright_pts() needs to be applied with a relatively aggressive
-percentile (default: 98) so as not to be caught by a long outlying tail.
-I.e. suppose red is relatively dark as compared to blue, but red has a longer
-bright tail.  A modest cut at the 99.9th percentile may end up brightening
-blue more than red.  Therefore, the action of color_bright_pts is not
-reversible and should not be applied if color will be corrected later by hand.
-The method ColorCorrect.correct() includes color_bright_pts().
+These routines were developed through experience of adjusting Planet and
+Landsat imagery by hand, using GIMP color curves. Scaling bright / dark points directly to percentiles on the image histogram leads to either (a) adjustments that tend toward posterization; or (b) adjustments based on histogram tails.  Therefore, the strategy is to find the (by default) (1, 99) percentiles as initial reference points and then step back by factor cut_frac.  I.e. instead of scaling the 1st percentile of the histogram to black, we find the pixel value of the first percentile, mutiply that value by cut_frac, and rescale the resulting pixel value to black. For one thing, this prevents over-saturating the image if there are no true blacks or whites in the scene.  
+
+To balance colors, it is even more important to operate with reference to the meat of the histogram, not out at the tail. Suppose red is relatively dark as compared to blue, but red has a longer bright tail.  A modest cut at the 99.9th percentile may end up brightening blue more than red.  Therefore, by default, we take the 95th percentile as initial reference and again step back by cut_frac.
+
+While _expand_histogram and _adjust_contrast can be reversed, roughly, by inverting the gamma transform and recompressing the histogram, the relative color blance cannot be reversed without knowing the initial color balance in the image, and therefore _balance_color should not be applied if color will be corrected later by hand.  This distinction is expressed in the difference between the correct() and brightness_and_contrast() methods.
 
 """
 import numpy as np
@@ -42,75 +41,87 @@ class ColorCorrect(object):
     """Perform basic color correction on an image.
 
     Attributes:
-        percentiles: high/low percentile cutoffs for full histogram
-        color_percentile: high cutoff for an individual color band
-        gamma: gamma correction exponent
+        percentiles: high/low histogram reference points for determining
+            cutoffs; default: (1, 99)
+        color_percentile: high reference point for an individual color band;
+            default: 95
+        cut_frac: factor by which to shift reference pixel values before
+            applying cutoffs (see notes above); default: .75
+        gamma: gamma correction exponent; default: .75
 
-    Methods:
+    External methods:
         brightness_and_contrast: Rescale intensities and enhance contrast.
         correct: Rescale intensities, enhance contrast, and balance colors.
 
     """
         
     def __init__(self,
-                 percentiles=(.1, 99.9),
-                 color_percentile=98,
-                 gamma=.8):
+                 percentiles=(1,99),
+                 color_percentile=95,
+                 cut_frac=.75,
+                 gamma=.75):
         self.percentiles = percentiles
         self.color_percentile = color_percentile
+        self.cut_frac = cut_frac
         self.gamma = gamma
 
     def correct(self, img):
         """Rescale image intensities, enhance contrast, and balance colors."""
-        img = white_pt_black_pt(img, self.white_black_percentiles)
-        img = adjust_contrast(img, gamma=self.gamma)
-        img = color_bright_pts(img, percentile=self.color_percentile)
+        print('Shifting white and black points.')
+        img = self._expand_histogram(img)
+        print('Adjusting contrast.')
+        img = self._adjust_contrast(img)
+        print('Equalizing color bright points.')
+        img = self._balance_colors(img)
         return img
 
     def brightness_and_contrast(self, img):
         """Rescale image intensities and enhance contrast."""
-        img = white_pt_black_pt(img, self.percentiles)
-        img = adjust_contrast(img, gamma=self.gamma)
+        img = self._expand_histogram(img)
+        img = self._adjust_contrast(img)
+        return img
+
+    def _expand_histogram(self, img):
+        """Expand histogram reference values to white and black."""
+        img_max = self._get_max(img)
+        lowcut, highcut = np.percentile(img[np.where(img > 0)],
+                                        self.percentiles)
+        lowcut = self._renorm_lowcut(lowcut)
+        highcut = self._renorm_highcut(highcut, img_max)
+        expanded = exposure.rescale_intensity(img, in_range=(lowcut, highcut))
+        return expanded
+
+    def _adjust_contrast(self, img):
+        """Peform gamma adjustment."""
+        return exposure.adjust_gamma(img, gamma=self.gamma)
+
+    def _balance_colors(self, img):
+        """Shift reference value to max brightness for each color channel."""
+        img_max = self._get_max(img)
+        for n, band in enumerate(img.T):
+            highcut = np.percentile(band[np.where(band > 0)],
+                                    self.color_percentile)
+            highcut = self._renorm_highcut(highcut, img_max)
+            img.T[n] = exposure.rescale_intensity(band, in_range=(0, highcut))
         return img
     
-def white_pt_black_pt(img, percentiles):
-    """Rescale given percentiles of histogram to white and black.
+    def _renorm_lowcut(self, cut):
+        """Shift cut toward zero."""
+        return cut * self.cut_frac
 
-    Arguments:
-        img type: ndarray
-        percentiles: tuple of floats, e.g. (.1, 99.9)
+    def _renorm_highcut(self, cut, img_max):
+        """Shift cut toward img_max."""
+        shifted = img_max - self.cut_frac * (img_max - cut)
+        if img_max == 255.:
+            shifted = round(shifted)
+        return shifted
 
-    Returns: ndarray
-    """
-    print('Rescaling white and black points.')
-    lowcut, highcut = np.percentile(img[np.where(img > 0)], percentiles)
-    return exposure.rescale_intensity(img, in_range=(lowcut, highcut))
-
-def adjust_contrast(img, gamma):
-    """Peform gamma adjustment.
-
-    Arguments:
-        img type: ndarray
-        gamma type: float (gamma < 1 brightens; gamma > 1 darkens)
-
-    Returns: ndarray
-    """
-    print('Adjusting contrast.')
-    return exposure.adjust_gamma(img, gamma=gamma)
-
-def color_bright_pts(img, percentile):
-    """Rescale given percentile of histogram to max brightness for each
-    color channel separately.
-
-    Arguments:
-        img type: ndarray
-        percentile: float (e.g.: 98)
-
-    Returns: ndarray
-    """
-    print('Equalizing color bright points.')
-    for n, band in enumerate(img.T):
-        highcut = np.percentile(band[np.where(band > 0)], percentile)
-        img.T[n] = exposure.rescale_intensity(band, in_range=(0, highcut))
-    return img
-    
+    def _get_max(self, img):
+        """Determine the maximum allowed pixel value for image."""
+        if img.dtype == 'uint8':
+            img_max = 255.
+        elif img.dtype == 'float32':
+            img_max = 1.0
+        else:
+            raise TypeError("Image dtype 'uint8' or 'float32' expected.")
+        return img_max
