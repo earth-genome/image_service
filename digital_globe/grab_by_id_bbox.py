ba900b173ecd9d8da0bac10e0bd1064a245d78ca
Ed Boyda <ed@space.ninja>
2018-04-30 22:51:19 -0700

    minor tweaks to bring old cli files up to date

--- digital_globe/grab_by_id_bbox.py
+++ digital_globe/grab_by_id_bbox.py
@@ -7,29 +7,26 @@ Jupyter @ notebooks.geobigdata.io.
 
 Ref: http://gbdxtools.readthedocs.io/en/latest/image_classes.html
 
-Image specs (default):  Atmosphere corrected, RGB bands
-
-Inputs: CatalogID, bounding box, flag option (@e) to equalize histograms,
+Inputs: CatalogID, bounding box, flag option (@a) for atmospheric compensation,
     flag option (@p) to pansharpen imagery.
     Note: flag is set with @ in place of - to allow input of negative
     numbers.
     Note: Pansharpening implies a four-fold increase in resolution and
     corresponding increase in tile downloads.
 
-Output: PNG file(s). (With @e flag, both original and equalized are saved.)
+Output: Dynamical-range-adjusted RGB PNG image file.
 
-Usage: python grab_by_id_bbox.py CatalogID  bounding box [@e] [@p]
+Usage: python grab_by_id_bbox.py CatalogID  bounding box [@a] [@p]
 
-Ex: python pansharp.py 1040010034CDD100 151.269378, -33.898346, 151.286458, -33.886092 @e 
+Ex: python grab_by_id_bbox.py 1040010034CDD100 151.269378, -33.898346, 151.286458, -33.886092 @p
 """
 import numpy as np
 import argparse
-import cv2
 import skimage
 import matplotlib.pyplot as plt
 from gbdxtools import CatalogImage
 
-def grab_rgb(catalog_id, bbox, pansharpen=True, acomp=True):
+def grab_rgb(catalog_id, bbox, pansharpen=True, acomp=False):
     """Grab an image from GBDX CatalogImage. Return the RGB."""
     img = CatalogImage(
         catalog_id, 
@@ -38,16 +35,6 @@ def grab_rgb(catalog_id, bbox, pansharpen=True, acomp=True):
         acomp=acomp
     )   
     return img.rgb()
-
-    
-def equalize_histogram(img):
-    """Equalize histogram of input image."""
-    if img.dtype is not np.dtype('uint8'):
-        img = skimage.img_as_ubyte(img)
-    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-    hsv[:,:,2] = cv2.equalizeHist(hsv[:,:,2])
-    equalized = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-    return equalized
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prefix_chars='@',
@@ -63,14 +50,14 @@ if __name__ == '__main__':
             '151.269378, -33.898346, 151.286458, -33.886092')
     )
     parser.add_argument(
-        '@e', '@@equalize_histogram',
-        dest='equalize',
+        '@p', '@@pansharpen',
+        dest='pansharpen',
         action='store_true',
         help='Flag (True if set / no value required.)'
     )
     parser.add_argument(
-        '@p', '@@pansharpen',
-        dest='pansharpen',
+        '@a', '@@acomp',
+        dest='acomp',
         action='store_true',
         help='Flag (True if set / no value required.)'
     )
@@ -81,10 +68,7 @@ if __name__ == '__main__':
         args.catalog_id, 
         bbox=bbox, 
         pansharpen=args.pansharpen,
-        acomp=True
+        acomp=args.acomp
     )
     plt.imsave(outfile + '.png', rgb)
-    if args.equalize:
-        rgb = equalize_histogram(rgb)
-        plt.imsave(outfile+'-equi.png', rgb)
    
