e72a694250ae8a523238dd1a8959daa62373c9e5
Ed Boyda <ed@space.ninja>
2018-04-30 22:51:19 -0700

    minor tweaks to bring old cli files up to date

--- digital_globe/grab_by_latlon.py
+++ digital_globe/grab_by_latlon.py
@@ -1,3 +1,4 @@
+
 """Download an image from the DG catalog given lat/lon and scale.
 
 Inputs:
@@ -6,19 +7,23 @@ Inputs:
 
 Usage:
     python grab_by_latlon.py 38.8977 -77.0365 -s 3.5
-    Outputs: PNG image
+    Outputs: Dynamical-range-adjusted RGB PNG image
 
-Parameters assumed: offNadirAngle=None, acomp=True, proj='EPSG:4326'
-Image source (incl. sensor and pansharpening) determined by default.
-For more flexibility, see/use dg_grabber.py.
+Image source (incl. sensor and pansharpening) determined automatically;
+parameters other than clouds and start/end dates conform to defaults
+set in dg_grabber.py  For more flexibility, see/use dg_grabber.py.
 
-For command line syntax with additional supported option:
+For command line syntax with additional supported options:
 python grab_draft.py -h
 
 """
 
 import argparse
-import dg_grabber
+import sys
+
+sys.path.append('../')
+from digital_globe import dg_grabber
+from geobox import geobox
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -39,7 +44,7 @@ if __name__ == '__main__':
         type=float,
         default=1.0,
         help='Approx. image size (width or height) in km, ' +
-            'e.g.: {}'.format(1.0)
+            'default: {}'.format(1.0)
     )
     parser.add_argument(
         '-N', '--N_images',
@@ -50,21 +55,23 @@ if __name__ == '__main__':
     parser.add_argument(
         '-c', '--clouds',
         type=int,
-        default=10,
-        help='Maximum percentage cloud cover, default: {}'.format(10)
+        default=dg_grabber.DEFAULT_SPECS['clouds'],
+        help='Maximum percentage cloud cover, default: {}'.format(
+            dg_grabber.DEFAULT_SPECS['clouds'])
     )
     parser.add_argument(
         '-sd', '--startDate',
         type=str,
-        default='2012-01-01T09:51:36.0000Z',
+        default=dg_grabber.DEFAULT_SPECS['startDate'],
         help='Isoformat start date for image search, default: {}'.format(
-            '2012-01-01T09:51:36.0000Z')
+            dg_grabber.DEFAULT_SPECS['startDate'])
     )
     parser.add_argument(
         '-ed', '--endDate',
         type=str,
-        default=None,
-        help='Isoformat end date for image search (default None).'
+        default=dg_grabber.DEFAULT_SPECS['endDate'],
+        help='Isoformat end date for image search, default: {}'.format(
+            dg_grabber.DEFAULT_SPECS['endDate'])
     )
     parser.add_argument(
         '-f', '--file_header',
@@ -78,11 +85,11 @@ if __name__ == '__main__':
     scale = args.pop('scale')
     N_images = args.pop('N_images')
     file_header = args.pop('file_header')
-    bbox = dg_grabber.bbox_from_scale(lat, lon, scale) 
+    bbox = geobox.bbox_from_scale(lat, lon, scale)
     grabber = dg_grabber.DGImageGrabber(**args)
-    imgs, records = grabber(bbox, N_images=N_images, write_styles=['DRA'],
-                            file_header=file_header)
-    if len(imgs) == 0:
+    records = grabber(bbox, N_images=N_images, write_styles=['DRA'],
+                      file_header=file_header)
+    if len(records) == 0:
         print('Try expanding the date range, change scale to change ' +
                'sensor, or access dg_grabber.py for more options.')
     else:
