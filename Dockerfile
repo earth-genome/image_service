310c6404f27524c9e29f47f5022538d5070e5fd5
Ed Boyda <ed@space.ninja>
2018-05-03 14:04:26 -0700

    new python3 docker setup; new app.py; grab_imagery submodule

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM ubuntu:latest
 RUN apt-get update && apt-get install -y software-properties-common
-RUN apt-get install -y python-pip python-dev build-essential 
-RUN apt-get install -y gdal-bin python-gdal python3-gdal
-RUN apt-get install -y libssl-dev libffi-dev python-dev libcurl4-openssl-dev
-RUN apt-get install -y python-tk
+RUN apt-get install -y python3-pip python3-dev build-essential
+RUN pip3 install --upgrade pip
+RUN apt-get install -y gdal-bin libgdal-dev python3-gdal
+RUN apt-get install -y libssl-dev libffi-dev libcurl4-openssl-dev
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get install -y python3-tk
 
 ADD ./webapp/requirements.txt /tmp/requirements.txt
 
@@ -12,9 +14,9 @@ RUN pip install -r /tmp/requirements.txt
 
 # Add our code
 ADD ./webapp /opt/webapp/
-WORKDIR /opt/webapp	
+WORKDIR /opt/webapp
 
 # Run the app.  CMD is required to run on Heroku.  $PORT is set by Heroku.
 # CMD gunicorn --bind 0.0.0.0:$PORT wsgi --timeout 6000
 
-CMD python app.py
+CMD python3 app.py
--- webapp/app.py
+++ webapp/app.py
@@ -1,41 +1,78 @@
+
 import os
 import sys
-import math
 import json
 import datetime
-import requests
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from flask import Flask, request
-from geobox import geobox
-from shapely import wkt
 
-import gbdxtools
-
-
-# import dg_grabber
-
-
+sys.path.append('grab_imagery/')
+sys.path.append('grab_imagery/story-seeds/')
+import auto_grabber
+from digital_globe import dg_grabber
 
 app = Flask(__name__)
 
+@app.route('/search')
+def search():
+    usage_msg = ('Usage: http://base.url/search?' +
+                 'provider=digital_globe&lat=37.7749&lon=-122.4194' +
+                 '&start=2018-01-01&end=2018-05-02&N=5')
+    args = request.args
+    provider = args.get('provider', type=str)
+    lat = args.get('lat', type=float)
+    lon = args.get('lon', type=float)
+    startDate = args.get('start', type=str)
+    endDate = args.get('end', type=str)
+    N_records = args.get('N', default=10, type=int)
 
-@app.route('/dg/assets')
-def newswire():
+    if not lat or not lon:
+        raise ValueError('Lat, lon are required. {}'.format(usage_msg))
 
-
-    lon = float(request.args.get('lon'))
-    lat = float(request.args.get('lat'))
-    start = str(request.args.get('start'))
-    end = str(request.args.get('end'))
-
-	bbox = geobox.bbox_from_scale(37.77, -122.42, 1.0)
-	g = dg_grabber.DGImageGrabber(startDate=start, endDate=end)
-	_, _, records = g(bbox, N_images=10, write_styles=None)
-
-	return json.dumps(records[0])
-
+    if provider == 'digital_globe':
+        grabber = dg_grabber.DGImageGrabber(startDate=startDate,
+                                            endDate=endDate)
+        records = grabber.search_latlon_clean(lat, lon, N_records=N_records)
+    elif provider == 'planet':
+        return 'Planet imagery coming soon.'
+    else:
+        raise ValueError('Supported providers are {}.\n{}'.format(
+            list(auto_grabber.PROVIDERS.keys()), usage_msg))
+    return json.dumps(records)
+    
+@app.route('/pull')
+def pull():
+    usage_msg = ('Usage: http://base.url/pull?' +
+                 'lat=37.7749&lon=-122.4194&scale=1.0' +
+                 'provider=digital_globe&clouds=10' +  
+                 '&start=2018-01-01&end=2018-05-02&N=1\n' +
+                 'Lat, lon, and scale are required; give scale in km.')
+    args = request.args
+    lat = args.get('lat', type=float)
+    lon = args.get('lon', type=float)
+    scale = args.get('scale', type=float)
+    if not lat or not lon or not scale:
+        raise ValueError('Lat, lon, scale are required. {}'.format(
+            usage_msg))
+    
+    specs = {
+        'startDate': args.get('start', type=str),
+        'endDate': args.get('end', type=str),
+        'clouds': args.get('clouds', type=int),
+        'N_images': args.get('N', default=1, type=int)
+    }
+    specs = {k:v for k,v in specs.items() if v}
+    provider = args.get('provider', type=str)
+    if provider:
+        specs.update({'provider_names': [provider]})
+        
+    bbox = auto_grabber.geobox.square_bbox_from_scale(lat, lon, scale)
+    grabber = auto_grabber.AutoGrabber('bespoke-images', **specs)
+    records = grabber.pull(bbox)
+    return json.dumps(records)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
