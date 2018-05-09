a52e8a75a388854e6dc6d6fbb7a00cf4d7c45715
Ed Boyda <ed@space.ninja>
2018-05-08 21:53:02 -0700

    add functions to retrieve story record and pull images for story in WTL database

--- webapp/app.py
+++ webapp/app.py
@@ -11,12 +11,23 @@ from flask import Flask, request
 
 sys.path.append('grab_imagery/story-seeds/')
 from grab_imagery import auto_grabber
+from grab_imagery import firebaseio
 from grab_imagery.digital_globe import dg_grabber
+
+# newswire
+DB_CATEGORY = '/WTL'
+WIRE_BUCKET = 'newswire-images'
+
+# bespoke imagery
+BUCKET = 'bespoke-images'
+
 
 app = Flask(__name__)
 
 @app.route('/search')
 def search():
+    """Search image availability for give lat, lon."""
+    
     usage_msg = ('Usage: http://earthrise-assets.herokuapp.com/search?' +
                  'provider=digital_globe' +  
                  '&lat=37.7749&lon=-122.4194' +
@@ -50,6 +61,8 @@ def search():
     
 @app.route('/pull')
 def pull():
+    """Pull images given lat, lon, and scale."""
+    
     usage_msg = ('Usage: http://earthrise-assets.herokuapp.com/pull?' +
                  'provider=digital_globe' + 
                  '&lat=37.7749&lon=-122.4194&scale=1.0' +
@@ -82,8 +95,49 @@ def pull():
 
     specs.update({'bbox_rescaling': 1})
     bbox = auto_grabber.geobox.square_bbox_from_scale(lat, lon, scale)
-    grabber = auto_grabber.AutoGrabber('bespoke-images', **specs)
+    grabber = auto_grabber.AutoGrabber(BUCKET, **specs)
     records = grabber.pull(bbox)
+    return json.dumps(records)
+
+@app.route('/retrieve-story')
+def retrieve_story():
+    """Retrieve a story record from the WTL database."""
+    
+    STORY_SEEDS = firebaseio.DB(firebaseio.FIREBASE_URL)
+    
+    usage_msg = ('Usage: http://earthrise-assets.herokuapp.com' +
+                 '/retrieve-story?idx=Index of the story in the database')
+
+    idx = request.args.get('idx', type=str)
+
+    record = STORY_SEEDS.get(DB_CATEGORY, idx)
+    if not record:
+        raise ValueError('Story index not found.\n{}'.format(usage_msg))
+    story = firebaseio.DBItem(DB_CATEGORY, idx, record)
+    return json.dumps({story.idx: story.record})
+
+@app.route('/pull-for-story')
+def pull_for_story():
+    """Pull images for a story in the WTL database."""
+    
+    usage_msg = ('Usage: http://earthrise-assets.herokuapp.com' +
+                 '/pull-for-story?idx=Index of the story in the database' +
+                 '&N=3')
+
+    STORY_SEEDS = firebaseio.DB(firebaseio.FIREBASE_URL)
+
+    args = request.args
+    idx = args.get('idx', type=str)
+    N_images = args.get('N', default=3, type=int)
+
+    record = STORY_SEEDS.get(DB_CATEGORY, idx)
+    if not record:
+        raise ValueError('Story index not found.\n{}'.format(usage_msg))
+    story = firebaseio.DBItem(DB_CATEGORY, idx, record)
+
+    grabber = auto_grabber.AutoGrabber(WIRE_BUCKET, N_images=N_images)
+    records = grabber.pull_for_story(story)
+
     return json.dumps(records)
 
 if __name__ == '__main__':
