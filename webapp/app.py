6685e546db24771bfe29232b937482a027d1b21b
Ed Boyda <ed@space.ninja>
2018-05-18 23:08:54 -0700

    updates for planet pulling

--- webapp/app.py
+++ webapp/app.py
@@ -11,11 +11,8 @@ from flask import Flask, request, make_response
 
 sys.path.append('grab_imagery/story-seeds/')
 from grab_imagery import auto_grabber
+from grab_imagery.auto_grabber import PROVIDER_CLASSES
 from grab_imagery import firebaseio
-from grab_imagery.digital_globe import dg_grabber
-from grab_imagery.planet_labs import planet_grabber
-
-PROVIDERS = ['digital_globe', 'planet']
 
 # newswire
 DB_CATEGORY = '/WTL'
@@ -40,8 +37,12 @@ def help():
 
     msg += ('Search for available images based on lat, lon:'
             '&emsp;{}<br>'.format(''.join((request.url, 'search?'))))
+    msg += ('Retrieve record for a known catalog ID:'
+            '&emsp;{}<br>'.format(''.join((request.url, 'search-id?'))))
     msg += ('Pull images based on lat, lon, and scale:'
             '&emsp;{}<br>'.format(''.join((request.url, 'pull?'))))
+    msg += ('Pull image for a known catalogID:'
+            '&emsp;{}<br>'.format(''.join((request.url, 'pull-by-id?'))))
     msg += ('Retrieve a story record from the WTL database:'
             '&emsp;{}<br>'.format(''.join((request.url, 'retrieve-story?'))))
     msg += ('Pull images for a story in the WTL database:'
@@ -63,14 +64,35 @@ def search():
     except ValueError as e:
         return '{}<br>{}<br>'.format(repr(e), msg)
 
-    if provider == 'digital_globe':
-        grabber = dg_grabber.DGImageGrabber(**specs)
-    elif provider == 'planet':
-        grabber = planet_grabber.PlanetGrabber(**specs)
-        
+    grabber = PROVIDER_CLASSES[provider](**specs)
     records = grabber.search_latlon_clean(lat, lon,
                                           N_records=specs['N_images'])
     return json.dumps(records)
+
+@app.route('/search-id')
+def search_id():
+    """Retrieve catalog record for input catalogID."""
+    notes = ('(Provider and id are required; ' +
+             'for Planet the associated item_type also is required.)')
+    msg = _help_msg(
+        request.base_url,
+        ('provider=planet&id=1425880_1056820_2018-05-14_0f18' +
+        '&item_type=PSScene3Band'),
+        notes)
+
+    args = request.args
+    provider = args.get('provider', type=str)
+    catalogID = args.get('id', type=str)
+    item_type = args.get('item_type', type=str)
+    if not provider or not catalogID:
+        return msg
+    elif provider == 'planet' and not item_type:
+        return 'For Planet an item_type is required:<br>{}'.format(msg)
+
+    grabber = PROVIDER_CLASSES[provider]()
+    record = grabber.search_id(catalogID, item_type)
+    return json.dumps(record)
+
     
 @app.route('/pull')
 def pull():
@@ -83,16 +105,46 @@ def pull():
         provider, lat, lon, scale, specs = _parse_geoloc(request.args)
     except ValueError as e:
         return '{}<br>{}<br>'.format(repr(e), msg)
-
     if not scale:
         return 'Scale (in km) is required.<br>{}'.format(msg)
 
     specs.update({'bbox_rescaling': 1})
-    specs.update({'provider_names': [provider]})
+    specs.update({'providers': [provider]})
     bbox = auto_grabber.geobox.square_bbox_from_scale(lat, lon, scale)
     grabber = auto_grabber.AutoGrabber(BUCKET, **specs)
     records = grabber.pull(bbox)
     return json.dumps(records)
+
+@app.route('/pull-by-id')
+def pull_by_id():
+    """Pull an image for a known catalogID."""
+    notes = ('(All arguments but the last are required; ' + 
+             'if the provider is Planet then item_type is also required.)')
+    msg = _help_msg(
+        request.base_url,
+        (EXAMPLE_ARGS.split('&start')[0] +
+         '&scale=1.0&id=103001007D4FDC00&item_type=visual'),
+        notes)
+
+    try:
+        provider, lat, lon, scale, _ = _parse_geoloc(request.args)
+    except ValueError as e:
+        return '{}<br>{}<br>'.format(repr(e), msg)
+    catalogID = request.args.get('id', type=str)
+    item_type = args.get('item_type', type=str)
+    if not scale:
+        return 'Scale (in km) is required.<br>{}'.format(msg)
+    if not catalogID:
+        return 'Catalog id is required.<br>{}'.format(msg)
+    if provider == 'planet' and not item_type:
+        return 'For Planet an item_type is required:<br>{}'.format(msg)
+    
+    bbox = auto_grabber.geobox.square_bbox_from_scale(lat, lon, scale)
+    grabber = auto_grabber.AutoGrabber(
+        BUCKET, providers=[provider], N_images=1)
+    record = grabber.pull_by_id(provider, bbox, catalogID, item_type)
+    return json.dumps(record)
+    
 
 @app.route('/retrieve-story')
 def retrieve_story():
@@ -135,9 +187,9 @@ def _parse_geoloc(args):
     scale = args.get('scale', type=float)
 
     provider = args.get('provider', type=str)
-    if not provider or provider not in PROVIDERS:
+    if not provider or provider not in PROVIDER_CLASSES.keys():
         raise ValueError('A provider is required; supported providers ' +
-                         'are {}'.format(PROVIDERS))
+                         'are {}'.format(PROVIDER_CLASSES.keys()))
     
     specs = {
         'startDate': args.get('start', type=str),
