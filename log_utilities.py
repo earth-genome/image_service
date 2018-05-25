e6ec21240f802fe94cb9757074e9501b716ddb3a
Ed Boyda <ed@space.ninja>
2018-05-24 21:20:30 -0700

    add function to log to stderr or stdout

--- log_utilities.py
+++ log_utilities.py
@@ -3,6 +3,12 @@ import logging
 import os
 import sys
 
+def get_stream_logger(stream):
+    logger = logging.getLogger()
+    sh = logging.StreamHandler(stream)
+    logger.addHandler(sh)
+    return logger
+
 def build_logger(directory, logfile, logger_name=__name__):
     logger = logging.getLogger(logger_name)
     if not os.path.exists(directory):
