98814da6ee0390f66dda32f5b716498b9b509ba5
Ed Boyda <ed@space.ninja>
2018-09-13 13:07:02 -0700

    allow collections with already-UTM-projected features (e.g. output from QGIS)
    Former-commit-id: 3d8ec665c0f0c5c243dc64bcbd51cfb04d8f2e76

--- geobox/areas.py
+++ geobox/areas.py
@@ -9,7 +9,7 @@ import shapely.geometry
 
 import projections
 
-def compute_area(feature, epsg_code=None):
+def compute_area(feature, projection=None):
     """Compute area of a geojson Feature given in geographic coordinates.
     
     If epsg_code is None, a UTM zone is inferred from the geometry centroid.
@@ -17,22 +17,30 @@ def compute_area(feature, epsg_code=None):
     Arguments:
         feature: A geojson Feature. Coordinates are specified 
             [[[lon1, lat1, z1], [lon2, lat2, z2], ...]]
-        epsg_code: Integer component of an EPSG code, e.g. 32601
+        projection: Integer component of an EPSG code, e.g. 32601, or 1
+            to indicate that a UTM code should be computed
     """
-    if not epsg_code:
-        centroid = shapely.geometry.asShape(feature['geometry']).centroid
-        lon, lat = centroid.x, centroid.y
-        epsg_code = projections.get_utm_code(lat, lon)
+    if projection:
+        if projection == 1:
+            centroid = shapely.geometry.asShape(feature['geometry']).centroid
+            lon, lat = centroid.x, centroid.y
+            epsg_code = projections.get_utm_code(lat, lon)
+        else:
+            epsg_code = projection
+        projected_geom = _project(feature['geometry'], epsg_code=epsg_code)
+    else:
+        projected_geom = feature['geometry']
 
-    projected_geom = _project(feature['geometry'], epsg_code=epsg_code)
     polygon = shapely.geometry.asShape(projected_geom)
     return polygon.area
 
-def compute_areas(feature_collection_fname, overwrite=True):
+def compute_areas(feature_collection_fname, projection=None, overwrite=True):
     """Compute areas for all features in a FeatureCollection file.
 
     Arguments:
         feature_collection_fname: Filename for a GeoJSON Feature Collection
+        projection: Integer component of an EPSG code, or 1 to indicate
+            that a UTM projection should be determined.
         overwrite: If True, overwite input file to include computed areas
 
     Returns: List of areas for features in the collection
@@ -42,12 +50,15 @@ def compute_areas(feature_collection_fname, overwrite=True):
 
     areas = []
     for feature in geojsons['features']:
-        area = compute_area(feature)
-        feature['properties'].update({'area': '{:.2g}'.format(area)})
+        area = compute_area(feature, projection=projection)
+        feature['properties'].update({'area': '{:.3g}'.format(area)})
         areas.append(area)
 
     if overwrite:
-        geojsons.update({'Total area': '{:.2g}'.format(np.sum(areas))})
+        geojsons.update({
+            'Total area': '{:.2g}'.format(np.sum(areas)),
+            'Number of features': '{}'.format(len(areas))
+        })
         with open(feature_collection_fname, 'w') as f:
             json.dump(geojsons, f, indent=4)
     return areas
@@ -68,9 +79,16 @@ if __name__ == '__main__':
         description='Compute areas of GeoJSON features, using UTM projections.'
     )
     parser.add_argument(
-        '-f', '--filename',
+        'filename',
         type=str,
         help='A filename for a GeoJSON FeatureCollection'
+    )
+    parser.add_argument(
+        '-p', '--projection',
+        type=int,
+        help=('Integer component of an EPSG code, or 1 to have the ' +
+              'relevant UTM code computed. If not given, the input ' + 
+              'coordinates will be presumed to be already projected.')
     )
     parser.add_argument(
         '-o', '--overwrite',
@@ -78,7 +96,9 @@ if __name__ == '__main__':
         help=('Flag. If set, input json will be overwitten to include ' +
               'computed areas.')
     )
-    args = parser.parse_args()
-    print(vars(args))
-    areas = compute_areas(args.filename, overwrite=args.overwrite)
-    print('Areas: {}\nTotal: {:.2g}'.format(areas, np.sum(areas)))
+    args = vars(parser.parse_args())
+    print(args)
+    filename = args.pop('filename')
+    areas = compute_areas(filename, **args)
+    print('Areas: {}'.format(areas))
+    print('Number: {}\nTotal area: {}'.format(len(areas), np.sum(areas)))
