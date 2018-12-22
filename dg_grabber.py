b2867b172939f94885382638913fadc7daf7cce6
Edward Boyda <boyda@earthrise.media>
2018-12-22 00:01:14 -0800

    Major refactoring of image grabbing: reconcile DG and Planet via new abstract base class ImageGrabber, with provider-specific details left to descendants.

---
+++ dg_grabber.py
@@ -0,0 +1,283 @@
+"""Class to automate searching and downloadling from the DigitalGlobe catalog.
+
+API ref: http://gbdxtools.readthedocs.io/en/latest/index.html
+
+Class DGImageGrabber: Descendant of class grabber.ImageGrabber
+
+Normally this class will be deployed via the GrabberHandler in
+grabber_handlers.py. Usage directly from this module with default specs: 
+
+> from utilities.geobox import geobox
+> bbox = geobox.bbox_from_scale(37.77, -122.42, 1.0)
+> g = DGImageGrabber()
+> g(bbox)
+
+Catalog and image specs have defaults set in default_specs.json, and can be 
+overriden by passing **kwargs to DGImageGrabber. As of writing, the
+DG-relevant default specs take form:
+{
+    "clouds": 10,   # maximum allowed percentage cloud cover
+    "min_intersect": 0.9,  # min fractional overlap between bbox and scene
+    "startDate": "2008-09-01T00:00:00.0000Z",  # for catalog search
+    "endDate": null,  # for catalog search
+    "N_images": 1,
+    "skip_days": 0, # min days between scenes if N_images > 1
+    "write_styles": [
+        "base",
+        "vibrant"
+    ],
+    "landcover_indices": [],
+    "thumbnails": false,
+    "file_header": ""
+    "offNadirAngle": null,   # (relation, angle), e.g. ('<', 10)
+    "band_type": "MS",  # mulit-spectral
+    "pansharp_scale": 2.5,  # in km; used by _patch_geometric_specs(),
+        which sets pansharpen=True below this scale
+    "acomp": false,
+    "override_proj": null, # any EPSG code, e.g. "EPSG:4326"; if null, a UTM
+        projection is determined from the bbox
+    "image_source": [
+	    "WORLDVIEW02",
+	    "WORLDVIEW03_VNIR",
+	    "GEOEYE01"
+    ]
+}
+
+The parameter image_source is from
+['WORLDVIEW02', 'WORLDVIEW03_VNIR', 'GEOEYE01', 'QUICKBIRD02', 'IKONOS'].
+The first three are are fairly comparable in resolution
+(.3-.5 meters/pixel if pansharpened) and are currently active.
+The latter two have resolution roughly half that and we decomissioned in 2015.
+
+Two parameters are determined within _patch_geometric_specs(self, bbox) and
+added to self.specs during the image pull:
+
+- pansharpen: True or False according to whether image is smaller or
+larger than pansharp_scale.
+
+- proj: In principle this could be any EPSG code, e.g. EPSG:4326, and can
+be set as such by setting override_proj='EPSG:4326'. Generically, here, it
+will be the Universal Transverse Mercator (UTM) projection appropriate for
+the bbox.
+
+
+"""
+
+import asyncio
+
+import dateutil
+import numpy as np
+import shapely
+import gbdxtools  # bug in geo libraries.  import this *after* shapely
+import rasterio # this too
+
+import grabber 
+from utilities.geobox import geobox
+from utilities.geobox import projections
+
+KNOWN_IMAGE_SOURCES = ['WORLDVIEW02', 'WORLDVIEW03_VNIR', 'GEOEYE01',
+                      'QUICKBIRD02', 'IKONOS']
+
+class DGImageGrabber(grabber.ImageGrabber):
+    """Tool to pull DigitalGlobe imagery.
+
+    External attributes and methods are defined in the parent ImageGrabber. 
+    """
+    
+    def __init__(self, client=gbdxtools.catalog.Catalog(), **specs):
+        super().__init__(client, **specs)
+        self._enforce_date_format()
+        self._search_filters = self._build_search_filters()
+
+
+    # Initializations to DG requirments:
+    
+    def _build_search_filters(self):
+        """Build filters to search catalog."""
+        sensors = ("(" + " OR ".join(["sensorPlatformName = '{}'".format(
+            source) for source in self.specs['image_source']]) + ")")
+        filters = [sensors]
+        filters.append('cloudCover < {:d}'.format(int(self.specs['clouds'])))
+        if self.specs['offNadirAngle']:
+            filters.append('offNadirAngle {} {}'.format(
+                self.specs['offNadirAngle']))
+        return filters
+
+    def _enforce_date_format(self):
+        """Re-assign dates in DG-approved format.
+    
+        The required format is 'YYYY-MM-DDTHH:MM:SS.XXXXZ'.
+        """
+        for date in ('startDate', 'endDate'):
+            if self.specs[date]: 
+                parsed = dateutil.parser.parse(self.specs[date])
+                formatted = parsed.isoformat(timespec='milliseconds')
+                formatted = formatted.split('+')[0] + 'Z'
+                self.specs[date] = formatted
+
+
+    # Search and scene preparation.
+    
+    def _search(self, bbox):
+        """Search the catalog for relevant imagery."""
+        records = self.client.search(
+            searchAreaWkt=bbox.wkt, filters=self._search_filters,
+            startDate=self.specs['startDate'],
+            endDate=self.specs['endDate'])
+        records.sort(key=lambda r: r['properties']['timestamp'], reverse=True)
+        print('Search found {} records.'.format(len(records)), flush=True) 
+        return records
+
+    def _search_id(self, catalogID, *args):
+        """Retrieve record for input catalogID."""
+        return self.client.get(catalogID)
+            
+    def _clean_record(self, record):
+        """Streamline image record."""
+        keymap = {  
+            'vendor': 'provider',
+            'sensorPlatformName': 'sensor',
+            'catalogID': 'catalogID',
+            'timestamp': 'timestamp',
+            'cloudCover': 'clouds',
+            'panResolution': 'resolution',
+            'browseURL': 'thumbnail'
+        }
+        cleaned = {keymap[k]:v for k,v in record['properties'].items()
+                   if k in keymap.keys()}
+        return cleaned   
+
+    def _compile_scenes(self, bbox, records):
+        """Retrieve dask images from the catalog.
+
+        As Digital Globe tiles are large and sparse, we do not attempt to 
+        gather tiles for mosaicking. Rather the challenge is that the API
+        will refuse us many tiles we request. A scene here is a list 
+        containing a single record, whose assets have been successfully 
+        accessed from the DG API.
+
+        Output: Cleaned records, including retrieved dask images.
+
+        Returns: A list of lists, each containing one record.
+        """
+        self._patch_geometric_specs(bbox)
+        retrieved = []
+        while len(records) > 0 and len(retrieved) < self.specs['N_images']:
+            rec = records.pop()
+            if not self._well_overlapped(bbox, [rec]):
+                continue
+            ID, props = rec['identifier'], rec['properties']
+            print('Trying ID {}:\n {}, {}'.format(
+                ID, props['timestamp'], props['sensorPlatformName']))
+            try:
+                daskimg = gbdxtools.CatalogImage(ID, **self.specs)
+                print('Retrieved ID {}'.format(ID))
+            except Exception as e:
+                print('Exception: {}'.format(e))
+                continue
+
+            overlap = bbox.intersection(self._read_footprint(rec))
+            rec.update({'daskimg': daskimg.aoi(bbox=overlap.bounds)})
+            retrieved.append([rec])
+            
+            if self.specs['skip_days']:
+                date = dateutil.parser.parse(props['timestamp']).date()
+                self._fastforward(records, date)
+
+        print('Found {} images of {} requested.'.format(
+            len(retrieved), self.specs['N_images']), flush=True)
+        return retrieved
+
+    def _patch_geometric_specs(self, bbox):
+        """Determine pansharpening and geoprojection."""
+        if self.specs['override_proj']:
+            proj = self.specs['override_proj']
+        else:
+            epsg = projections.get_utm_code(bbox.centroid.y, bbox.centroid.x)
+            proj = 'EPSG:{}'.format(epsg)
+            
+        pansharpen = self._check_highres(bbox)
+        self.specs.update({'proj': proj, 'pansharpen': pansharpen})
+
+    def _check_highres(self, bbox):
+        """Allow highest resolution when bbox smaller than pansharp_scale."""
+        size = np.mean(geobox.get_side_distances(bbox))
+        return True if size < self.specs['pansharp_scale'] else False
+    
+    def _read_footprint(self, record):
+        return shapely.wkt.loads(record['properties']['footprintWkt'])
+
+
+    # Scene download
+
+    async def _download(self, bbox, scene):
+        """Download scene assets.
+
+        Output: GeoTiff written to disk
+
+        Returns: List containing the path to the GeoTiff
+        """
+        record = next(iter(scene))
+        daskimg = record['daskimg']
+        bands = self._get_bandmap(daskimg.shape[0])
+        if not self.specs['landcover_indices']:
+            bands = bands[:3]
+
+        path = self._build_filename(bbox, record)
+        print('\nStaging at {}\n'.format(path), flush=True)
+        daskimg.geotiff(path=path, bands=bands, **self.specs)
+        path = expand_histogram(path)
+        return [path]
+
+    def _get_bandmap(self, n_bands):
+        """Retrieve the band ordering for R-G-B-NIR bands."""
+        bandmaps = {
+            '4': [2, 1, 0, 3],
+            '8': [4, 2, 1, 6]
+        }
+        return bandmaps[str(n_bands)]
+
+    def _build_filename(self, bbox, record):
+        """Build a filename for image output."""
+        tags = ('bbox{:.4f}_{:.4f}_{:.4f}_{:.4f}'.format(*bbox.bounds))
+        filename = (self.specs['file_header'] + record['identifier'] + '_' +
+                    record['properties']['timestamp'] + tags + '.tif')
+        return filename
+
+
+# Function to regularize raw DG GeoTiffs
+
+def expand_histogram(geotiff, percentile=97, target_value=8e3):
+    """Convert to uint16 and do a rough bandwise histogram expansion.
+
+    The dtype kwarg to DG img.geotiff method functions only for Worldview
+    images. Across all sensors, allowing default dytpe, images come as 
+    float32 with a uint16-like value range or as uint16. I have observed pixel
+    values larger than 2**14, but not as of yet larger than 2**16,
+    and generally the histogram is concentrated in the first
+    twelve bits. Relative R-G-B weightings are not reproduced consistently
+    from image to image of the same scene.
+
+    This function does a rough histogram expansion, finding the pixel 
+    value at the given percentile for each band and resetting that to the 
+    target_value. This allows the routines in postprocessing.color to be 
+    applied with the same parameters to both DG and Planet Labs images.
+
+    Output: Overwrites an expanded, uint16 GeoTiff
+
+    Returns: Path to the GeoTiff
+    """
+    with rasterio.open(geotiff) as dataset:
+        profile = dataset.profile.copy()
+        img = dataset.read()
+        coarsed = np.zeros(img.shape, dtype='uint16')
+        for n, band in enumerate(img):
+            cut = np.percentile(band[np.where(band > 0)], percentile)
+            coarsed[n] = ((band / cut) * target_value).astype('uint16')
+            
+    profile['dtype'] = 'uint16'
+    profile['photometric'] = 'RGB'
+    with rasterio.open(geotiff, 'w', **profile) as f:
+        f.write(coarsed)
+        
+    return geotiff
---
+++ grabber.py
@@ -0,0 +1,234 @@
+"""Abstract base class to automate search and download of satellite imagery.
+
+External class methods are defined in the template class ImageGrabber, here,
+and provider-dependent details expressed in its descendants 
+dg_grabber.DGImageGrabber and planet_grabber.PlanetGrabber.
+
+"""
+ 
+from abc import ABC, abstractmethod
+import asyncio
+import datetime
+import json
+import os
+
+import dateutil
+import shapely
+
+from postprocessing import color
+from postprocessing import gdal_routines
+from postprocessing import landcover
+from postprocessing import resample
+from utilities.geobox import geobox
+
+SPECS_FILE = os.path.join(os.path.dirname(__file__), 'default_specs.json')
+
+class ImageGrabber(ABC):
+    """Template for image grabbing.
+
+    Template attributes:
+        client: Satellite provider API instantiated interface  
+        specs: Dict of catalog and image specs
+
+    Template external methods:
+        __call__: Scheduling wrapper for async execution of pull()
+        async pull: Pull the most recent images consistent with specs.
+        async pull_by_id:  Grab and write image for a known catalogID.
+        prep_scenes: Search and group search records into scenes.
+        grab_scene: Activate, download, and process scene assets.
+        search_clean: Search and return streamlined image records.
+        search_latlon_clean:  Search and return streamlined image records.
+        search_id_clean: Retrieve record for input catalogID.
+        produce: Convert a raw GeoTiff into visual and data products.
+    """
+
+    def __init__(self, client, specs_filename=SPECS_FILE, **specs):
+        self.client = client
+        with open(specs_filename, 'r') as f:
+            self.specs = json.load(f)
+        self.specs.update(specs)
+
+        
+    # Top level image grabbing functions
+
+    def __call__(self, bbox):
+        """Scheduling wrapper for async execution of pull()."""
+        loop = asyncio.get_event_loop()
+        recs_written = loop.run_until_complete(self.pull(bbox))
+        return recs_written
+
+    async def pull(self, bbox):
+        """Pull the most recent images consistent with specs.
+    
+        Argument: bbox: a shapely box
+            
+        Returns: List of image records along with any exceptions
+        """
+        scenes = self.prep_scenes(bbox)
+        grab_tasks = [self.grab_scene(bbox, scene) for scene in scenes]
+
+        results = await asyncio.gather(*grab_tasks, return_exceptions=True)
+        return results
+
+    # WIP
+    async def pull_by_id(self, bbox, catalogID, *args):
+        pass
+        
+    async def grab_scene(self, bbox, scene):
+        """Activate, download, and process scene assets."""
+        paths = await self._download(bbox, scene)
+        merged_path, record = self._mosaic(bbox, paths, scene)
+        output_paths = self.produce(merged_path)
+        if self.specs['thumbnails']:
+            resample.make_thumbnails(output_paths)
+        record.update({'paths': output_paths})
+        return record
+
+    
+    # Search and scene preparation
+    
+    def prep_scenes(self, bbox):
+        """Search and group search records into scenes.
+
+        Returns: List of lists of records.  
+        """
+        records = self._search(bbox)[::-1]
+        scenes = self._compile_scenes(bbox, records)
+        return scenes
+    
+    @abstractmethod   
+    def _search(self):
+        pass
+
+    @abstractmethod
+    def _search_id(self):
+        pass
+
+    def _search_latlon(self, lat, lon, eps=.001):
+        """Search catalog for images containing lat, lon.
+
+        Argument eps: Scale in km for a small box around lat, lon.
+        """
+        minibox = geobox.bbox_from_scale(lat, lon, eps)
+        return self._search(minibox)
+    
+    def search_clean(self, bbox, max_records=None):
+        """Search the catalog and return streamlined records."""
+        records = self._search(bbox)
+        return [self._clean_record(r) for r in records[:max_records]]
+
+    def search_latlon_clean(self, lat, lon, max_records=None):
+        """Search the catalog and return streamlined records."""
+        records = self._search_latlon(lat, lon)
+        return [self._clean_record(r) for r in records[:max_records]]
+
+    def search_id_clean(self, catalogID, *args):
+        """Retrieve record for input catalogID."""
+        record = self._search_id(catalogID, *args)
+        return self._clean_record(record)
+
+    @abstractmethod
+    def _clean_record(self):
+        pass
+    
+    @abstractmethod
+    def _compile_scenes(self):
+        pass
+
+    def _well_overlapped(self, bbox, records):
+        """Check whether records meet specs for overlap with bbox."""
+        intersection = bbox.intersection(self._footprint(records))
+        fraction = intersection.area/bbox.area
+        well_o = (fraction >= self.specs['min_intersect'])
+        if not well_o:
+            print('Rejecting scene. Overlap with bbox {:.1%}'.format(fraction))
+        return well_o
+
+    def _footprint(self, records):
+        footprints = [self._read_footprint(r) for r in records]
+        return shapely.ops.cascaded_union(footprints)
+
+    @abstractmethod
+    def _read_footprint(self):
+        pass
+
+    def _fastforward(self, records, date):
+        """Pop records until all are older than date by specs['skip_days']
+
+        Arguments:
+            records: Image records sorted by date
+            date: Reference date to work back from
+
+        Output: Records are popped from input variable records.
+
+        Returns: None
+        """
+        target_date = date - datetime.timedelta(days=self.specs['skip_days'])
+        while records:
+            record = records.pop()
+            date_aq = dateutil.parser.parse(
+                record['properties']['acquired']).date()
+            if date_aq <= target_date:
+                records.append(record) # replace this record
+                break
+        return 
+
+    
+    # Scene activation and download
+
+    @abstractmethod
+    def _download(self):
+        pass
+
+    @abstractmethod
+    def _get_bandmap(self):
+        pass
+
+        
+    # Reprocessing
+
+    def _mosaic(self, bbox, paths, records):
+        """Assemble assets to geographic specs."""
+        merged = next(iter(paths))
+        record = self._clean_record(next(iter(records)))
+        return merged, record
+
+    def produce(self, path):
+        """Convert a raw GeoTiff into visual and data products."""
+        output_paths = []
+        if self.specs['landcover_indices']:
+            output_paths.append(self._indexing(path))
+            path = gdal_routines.reband(path, [1, 2, 3])
+        output_paths.append(self._coloring(path))
+
+        if self.specs['thumbnails'] and output_paths:
+            os.remove(path)
+        else:
+            output_paths.append(path)
+        return output_paths
+            
+    def _indexing(self, path):
+        """Compute landcover indices.
+
+        Returns: Paths to color-corrected images
+        """
+        output_paths = []
+        indices = [index.lower() for index in self.specs['landcover_indices']
+                   if index in landcover.INDICES]
+        for index in indices:
+            output_paths.append(landcover.compute_index(path, index))
+        return output_paths
+    
+    def _coloring(self, path):
+        """Produce styles of visual images.
+
+        Returns: Paths to color-corrected images
+        """
+        output_paths = []
+        styles = [style.lower() for style in self.specs['write_styles']
+                  if style in color.STYLES.keys()]
+        for style in styles:
+            output_paths.append(color.ColorCorrect(style=style)(path))
+        return output_paths
+
+    
