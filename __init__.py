6ede79b85ff07e8440157a7c70d1dbcbc9133d4e
Edward Boyda <boyda@earthrise.media>
2019-03-07 11:27:51 -0800

    update comment

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 Since this repo is to be used as a git submodule, add its directory
 to sys.path. Imports can be given with respect to packages in this
-directory: from digital_globe import dg_grabber
+directory: from postprocessing import color
 """
 
 from inspect import getsourcefile
