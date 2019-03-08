2458d05a3075fc28014405670db907087941eff5
Edward Boyda <boyda@earthrise.media>
2019-03-07 22:17:21 -0800

    replace skimage with rasterio to preserve geotiff headers

--- manual_processing/match_histograms.py
+++ manual_processing/match_histograms.py
@@ -2,8 +2,8 @@
 
 Usage: python match_histograms.py source_img.tif ref_img.tif
 
-Image types .png, .jpg are also supported.  It is assumed that pixel
-values in the two images have the same range, be it (0,255), (0., 1.), etc.  
+It is assumed that pixel values in the two images have the same range, 
+be it (0,255), (0., 1.), etc.  
 
 This routine operates on R, G, B bands in succession and does not offer
 matching in other color spaces as does rio hist.  
@@ -11,20 +11,14 @@ matching in other color spaces as does rio hist.
 """
 
 import argparse
-import sys
 
-import numpy as np
+import rasterio
 from rio_hist import match
-import skimage.io
-
-IMAGE_EXTS = ('tif', 'tiff', 'jpg', 'png')
 
 def parse_filename(filename):
     """Extract a prefix and extension from filename."""
     splits = filename.split('.')
     ext = splits[-1]
-    if ext not in IMAGE_EXTS:
-        sys.exit('Supported image files end in one of {}'.format(IMAGE_EXTS))
     prefix = '.'.join(splits[:-1])
     return prefix, ext
 
@@ -33,28 +27,30 @@ if __name__ == '__main__':
     parser.add_argument(
         'src_filename',
         type=str,
-        help='Filename of image to modify.  Formats: {}'.format(IMAGE_EXTS)
+        help='Filename of image to modify.'
     )
     parser.add_argument(
         'ref_filename',
         type=str,
-        help='Filename of image with histogram to match, also {}'.format(
-            IMAGE_EXTS)
+        help='Filename of image with histogram to match.'
     )
-    args = vars(parser.parse_args())
+    args = parser.parse_args()
 
-    src_prefix, src_ext = parse_filename(args['src_filename'])
-    _, ref_ext = parse_filename(args['ref_filename'])
-    
-    src = skimage.io.imread(args['src_filename'])
-    ref = skimage.io.imread(args['ref_filename'])
-    if src.dtype != ref.dtype:
-        sys.exit('Dtypes {}, {} do not match.'.format(src.dtype, ref.dtype))
-
+    with rasterio.open(args.src_filename) as f:
+        src = f.read()
+        profile = f.profile.copy()
+    with rasterio.open(args.ref_filename) as f:
+        ref = f.read()
+    assert src.dtype == ref.dtype
+    assert len(src) == len(ref)
+            
     matched = src.copy()
-    for band in range(3):
-        matched.T[band] = match.histogram_match(src.T[band], ref.T[band])
+    for band in range(len(src)):
+        matched[band] = match.histogram_match(src[band], ref[band])
 
-    skimage.io.imsave(src_prefix+'-matched.'+src_ext, matched)
+    src_prefix, src_ext = parse_filename(args.src_filename)
+    with rasterio.open(src_prefix+'-matched.'+src_ext, 'w', **profile) as f:
+        f.write(matched)
+
         
 
