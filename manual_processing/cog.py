85f4361bbf54796cd81aafdba23af1b491277f0b
Edward Boyda <boyda@earthrise.media>
2020-07-10 20:50:48 +0200

    add blocksize option

--- manual_processing/cog.py
+++ manual_processing/cog.py
@@ -120,8 +120,8 @@ def separate_bands(geotiff):
         outpaths.append(bandpath)
     return outpaths
 
-def make_cog(geotiff, profile='jpeg', mask=True, webmap=True, clean=False,
-             **kwargs):
+def make_cog(geotiff, profile='jpeg', mask=True, webmap=True, blocksize=512,
+             clean=False, **kwargs):
     """Convert geotiff into a Cloud-Optimized GeoTiff.
 
     Arguments: 
@@ -130,6 +130,7 @@ def make_cog(geotiff, profile='jpeg', mask=True, webmap=True, clean=False,
         mask: bool: To handle nodata values via a mask
         webmap: bool: The rio-cogeo web-optimized flag: Reprojects to 
             EPSG:3857 and aligns overviews to standard webmap tiles
+        blocksize: Integer tile size (typically 256 or 512)
         clean: bool: To delete the input file after processing
         **kwargs: Optional kwargs to pass to expand_histogram
         
@@ -140,6 +141,7 @@ def make_cog(geotiff, profile='jpeg', mask=True, webmap=True, clean=False,
     
     outpath = geotiff.split('.tif')[0] + '-cog.tif'
     commands = (f'rio cogeo create -p {profile} -r bilinear '
+                f'--co BLOCKXSIZE={blocksize} --co BLOCKYSIZE={blocksize} '
                 f'--overview-resampling bilinear {geotiff} {outpath}').split()
     if mask:
         commands += ['--add-mask']
