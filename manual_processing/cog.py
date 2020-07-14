1c484c023ab885385d9872a814cdbe9f96ef1d21
Edward Boyda <boyda@earthrise.media>
2020-07-14 14:59:40 +0200

    typo in prev commit

--- manual_processing/cog.py
+++ manual_processing/cog.py
@@ -114,7 +114,7 @@ def separate_bands(geotiff):
         outpaths.append(bandpath)
     return outpaths
 
-def make_cog(geotiff, profile='default', mask=False, webmap=True, tile_size=256,
+def make_cog(geotiff, profile='deflate', mask=False, webmap=True, tile_size=256,
              clean=False, **kwargs):
     """Convert geotiff into a Cloud-Optimized GeoTiff.
 
